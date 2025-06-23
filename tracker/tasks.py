1767b30f3f6e5827adad55d1adee41190defb217
otaviouu <otavio.venancio.oliveira@usp.br>
2025-06-22 21:46:30 -0300

    scrapizin

--- tracker/tasks.py
+++ tracker/tasks.py
@@ -4,6 +4,9 @@ import requests
 from parsel.selector import Selector
 import requests_html
 
+# quero importar modelo de busca
+from .models import Termo
+
 main_page_url = "https://www.estantevirtual.com.br/ciencias-exatas?sort=new-releases&tipo-de-livro=usado"
 # https://www.estantevirtual.com.br/frdmprcsts/J92-9149-000/lazy
 
@@ -13,10 +16,29 @@ def every_minute():
     session = requests_html.HTMLSession()
 
     response = session.get(main_page_url)
-    response.html.render(sleep=1, timeout=20)
+    response.html.render(sleep=3, timeout=20)
     selector = Selector(text=response.html.html)
-    titles = selector.css(
-        ".product-item__link.smarthint-tracking-card::attr(title)"
-    ).getall()
-    for title in titles:
-        print(title)
+
+    books = selector.css(".product-item.product-list__item")
+    all_terms = Termo.objects.all()
+    print(len(all_terms), "termos encontrados")
+    for book in books:
+        book_title = book.css("a::attr(title)").get()
+        print(f"Procurando livro: {book_title}")
+        if book_title:
+            for termo in all_terms:
+                if termo.name.lower() in book_title.lower():
+                    book_url = book.css("a::attr(href)").get()
+                    book_image_url = book.css(
+                        ".product-item__cover img::attr(src)"
+                    ).get()
+
+                    new_book_found = {
+                        "name": book_title,
+                        "url": book_url,
+                        "image_url": book_image_url,
+                    }
+                    print(new_book_found)
+                    termo.user.livros.append(new_book_found)
+                    termo.user.save()
+                    print(f"Livro encontrado: {book_title} para o termo {termo.name}")
--- users/models.py
+++ users/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, blank=False, null=False)
-
+    livros = models.JSONField(
+        blank=True,
+        default=list,
+    )
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
