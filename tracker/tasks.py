d5cf8fa3e4abdf8264277af1540bde7e1c745efa
otaviouu <otavio.venancio.oliveira@usp.br>
2025-06-29 00:06:24 -0300

    Refactor code structure for improved readability and maintainability

--- tracker/tasks.py
+++ tracker/tasks.py
@@ -6,6 +6,7 @@ import requests_html
 # quero importar modelo de busca
 from .models import Termo
 from urllib.parse import urljoin
+from seleniumbase import SB
 
 main_page_url = "https://www.estantevirtual.com.br/ciencias-exatas?sort=new-releases&tipo-de-livro=usado"
 # https://www.estantevirtual.com.br/frdmprcsts/J92-9149-000/lazy
@@ -13,12 +14,14 @@ main_page_url = "https://www.estantevirtual.com.br/ciencias-exatas?sort=new-rele
 
 @periodic_task(crontab(minute="*/1"))
 def every_minute():
-    session = requests_html.HTMLSession()
+    with SB(uc=False, headless=False) as sb:
+        sb.get(main_page_url)
+        sb.wait(seconds=3)
+        sb.slow_scroll_to(".payment-flags__title", timeout=10)
+        sb.slow_scroll_to()
+        html = sb.get_page_source()
 
-    response = session.get(main_page_url)
-    response.html.render(scrolldown=5, wait=4, timeout=20)
-
-    selector = Selector(text=response.html.html)
+    selector = Selector(text=html)
 
     books = selector.css(".product-item.product-list__item")
     all_terms = Termo.objects.all()
@@ -29,7 +32,11 @@ def every_minute():
             for termo in all_terms:
                 if termo.name.lower() in book_title.lower():
                     book_href = book.css("a::attr(href)").get()
-                    book_url = urljoin("https://www.estantevirtual.com.br", book_href) if book_href else None
+                    book_url = (
+                        urljoin("https://www.estantevirtual.com.br", book_href)
+                        if book_href
+                        else None
+                    )
                     book_image_url = book.css("img::attr(src)").get()
 
                     new_book_found = {
